bdbdd0314d87d8f09a446624ca8bb7e229598ee1
Zqarebaqi <zqarebaqi@gmail.com>
2025-09-30 05:32:33 +0330

    fix:fix bookings route

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ const App = () => {
           <Route element={<AppLayout />}>
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/booking" element={<Bookings />} />
+            <Route path="/bookings" element={<Bookings />} />
             <Route path="/cabins" element={<Cabins />} />
             <Route path="/users" element={<Users />} />
             <Route path="/settings" element={<Settings />} />
